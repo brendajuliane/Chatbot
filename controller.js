5632f12d3a6cf013d460e6e52585e37b78f54d27
Brenda Juliane <bre8juliane@hotmail.com>
2023-02-20 01:53:52 -0300

    Fix: only who uses #rf will enter the stages

--- controller.js
+++ controller.js
@@ -2,8 +2,6 @@ let storage = [];
 
 class controller {
     reply(client, message) {
-        
-        let stage = this.getStage(message.from);
 
         if (message.body === '#ping') {
             client.reply(message.from, 'pong', message.id.toString())
@@ -18,12 +16,15 @@ class controller {
                 client.sendImageAsSticker(message.from, result);
             })
             .catch((erro) => {
-                console.error('Error when sending: ', erro); //return object error
+                console.error('Error when sending: ', erro);
             });
         }
 
         if(message.body == '#rf') {
-            if(stage == "0") {
+
+            this.startStage(message.from);
+
+            if(this.getStage(message.from) == "0") {
                 storage[message.from].stage = "1";
 
                 client.sendListMessage(message.from, {
@@ -96,12 +97,23 @@ class controller {
             } 
         }
 
-        switch(stage) {
+        switch(this.getStage(message.from)) {
             case "1":
-                this.getDescription(message);
+                if(this.getCategory(message)) {
+                  client.sendText(message.from, "Digite o valor do registro (use ponto no lugar de vírgula):")
+                  .catch((erro) => {
+                      console.error('Error when sending: ', erro);
+                  });
+                } else {
+                  client.sendText(message.from, "Opção inválida, selecione outra vez")
+                  .catch((erro) => {
+                      console.error('Error when sending: ', erro);
+                  });
+                }
                 break;
 
             case "2":
+              
                 break;
 
             case "3":
@@ -111,7 +123,7 @@ class controller {
                 break;
 
             default:
-                console.log(`Stage is ${stage}`);
+                console.log(`Stage is ${this.getStage(message.from)}`);
                 break;
         }
     }
@@ -122,21 +134,26 @@ class controller {
     * 3 - waiting description
     * 4 - waiting confirmation
     */
-    getStage(from) {
-        if(storage[from]) {
-            return storage[from].stage;
-        } else {
-            storage[from] = {
-                stage: "0",
-                category: null,
-                value: null,
-                description: null
-            };
-            return storage[from].stage;
-        }
+    startStage(from) {
+      if(!storage[from]) {
+        storage[from] = {
+          stage: "0",
+          category: null,
+          value: null,
+          description: null
+        };
+      }
     }
 
-    getDescription(message) {
+    getStage(from) {
+      if(storage[from]) {
+          return storage[from].stage;
+      } else {
+          return null;
+      }
+    }
+
+    getCategory(message) {
         let choice = [message.body];
             let menuOptions = ['Dízimo', 'Oferta', 'Luz', 'Água', 'Internet', 'Celular', 'Viagem', 'Bus/metro', 'Taxi/Uber', 'Supermercado', 'Fastfood', 'Cuidado pessoal', 'Role', 'Outro'];
 
@@ -146,8 +163,10 @@ class controller {
             if(result.length === 1) {
                 storage[message.from].stage = "2";
                 storage[message.from].category = message.body;
+                console.log(storage);
+                return true;
             };
-            console.log(storage);
+            return false;        
     }
 }
 
